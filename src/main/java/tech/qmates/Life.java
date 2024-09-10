477cf702e5140b6bfd903223a5bc75d29ab1d0bb
Fabrizio Machella <fabrizio.machella@qmates.tech>
2024-09-10 22:03:30 +0200

    I can heal and damage the hitpoints that are immutable

--- src/main/java/tech/qmates/Life.java
+++ src/main/java/tech/qmates/Life.java
@@ -1,10 +1,31 @@
 package tech.qmates;
 
 public class Life {
+    private final int hitPoints;
+
     public Life(int hitPoints) {
+        this.hitPoints = hitPoints;
     }
 
-    public Life decrease(int decrease) {
-        return null;
+    public Life damage(int hitPoints) {
+        return new Life(this.hitPoints - hitPoints);
+    }
+
+    public Life heal(int hitPoints) {
+        return new Life(this.hitPoints + hitPoints);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Life)) return false;
+
+        Life life = (Life) o;
+        return hitPoints == life.hitPoints;
+    }
+
+    @Override
+    public int hashCode() {
+        return hitPoints;
     }
 }
--- src/test/java/tech/qmates/LifeTest.java
+++ src/test/java/tech/qmates/LifeTest.java
@@ -7,9 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class LifeTest {
 
     @Test
-    void life_decrease() {
+    void life_damage_decrease() {
         Life life = new Life(20);
-        Life remaining=life.decrease(10);
+        Life remaining=life.damage(10);
         assertEquals(remaining,new Life(10));
+    }
+
+    @Test
+    void life_heal_increase() {
+        Life life = new Life(20);
+        Life remaining=life.heal(10);
+        assertEquals(remaining,new Life(30));
     }
 }
